c31443d4ac706acd49fb8bb513a6d2a182804238
Maxim Dorofeev <Mektogon385@mail.ru>
2022-06-12 18:02:40 +0700

    Начало разработки микросервиса "Advertisement Management System".

---
+++ src/main/java/ru/dorofeev/sberbankproject/SberbankProjectApplication.java
@@ -0,0 +1,13 @@
+package ru.dorofeev.sberbankproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SberbankProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SberbankProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/dorofeev/sberbankproject/SberbankProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.dorofeev.sberbankproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SberbankProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
